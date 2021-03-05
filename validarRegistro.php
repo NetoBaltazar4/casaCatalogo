f56d0cff5a1c5318973447930c61f4c7eae84446
Neto <contacto@exoft.com>
2021-03-05 17:49:44 -0600

    Generar el id cuenta
    Se genera el id cuenta automaticamente

--- validarRegistro.php
+++ validarRegistro.php
@@ -9,14 +9,13 @@
      $banAm = 0;
      $banEmail = 0;
      $banPassword = 0;
-
 /////Validacion del nombre 
      if(isset($_POST['nombre'])){
           $nombre = $_POST['nombre'];
           if(preg_match('/^([A-ZÁÉÍÓÚ a-záéíóú]{4,20})*$/',$nombre)){
                  $nombreFilter = filter_var($nombre, FILTER_SANITIZE_STRING);
                  if(strlen($nombreFilter)>0 && trim($nombreFilter)){
-                      echo 'Paso la validacion: '.$nombreFilter;
+                    //   echo 'Paso la validacion: '.$nombreFilter;
                       $banNombre = 1;
 
                  }else{
@@ -46,7 +45,7 @@
           if(preg_match('/^([A-ZÁÉÍÓÚ a-záéíóú]{4,20})*$/', $ap)){
                $apFilter = filter_var($ap, FILTER_SANITIZE_STRING);
                if(strlen($apFilter)>0 && trim($apFilter)){
-                    echo 'Paso la validacion del apellido: '.$apFilter;
+                    // echo 'Paso la validacion del apellido: '.$apFilter;
                     $banAp = 1;
 
                }else{
@@ -67,7 +66,7 @@
                $amFilter = filter_var($am, FILTER_SANITIZE_STRING);
 
                if(strlen($amFilter) && trim($amFilter)){
-                    echo 'Paso la validacion '.$amFilter;
+                    // echo 'Paso la validacion '.$amFilter;
                     $banAm = 1;
                }else{
                     echo 'Ingresa tu Apellido correctamente';
@@ -86,7 +85,7 @@
           if(preg_match('/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/', $email)){
                $emailFilter = filter_var($email, FILTER_SANITIZE_STRING);
                if(strlen($emailFilter)>0 && trim($email)){
-                    echo 'Paso la validacion del correo';
+                    // echo 'Paso la validacion del correo';
                     $banEmail = 1;
 
                }else{
@@ -105,7 +104,7 @@
      if(isset($_POST['password'])){
           $password = $_POST['password'];
           if(preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#$^+=!*()@%&.;:,]).{8,20}$/',$password)){
-              echo 'Contrasenia correcta';
+          //     echo 'Contrasenia correcta';
               $banPassword = 1;
 
           }else{
@@ -115,31 +114,39 @@
      }
 
      if($banNombre == 1 && $banAp == 1  && $banAm == 1 && $banEmail == 1 && $banPassword == 1){
-               // $id = 5678;
-                $nombre = $_POST['nombre'];
-                $ap_user = $_POST['ap-registrar'];
-                $am_user = $_POST['am-registrar'];
-                $email = $_POST['email'];
-                $password = $_POST['password'];
-          
-               echo $nombre;
 
-               try{
+          try{
+               /////Mandamos llamr la conexion a base de datos 
+               require_once('includes/funciones/conexion.php');
+               $sql = 'SELECT * from registro_cuenta WHERE id_cuenta = (SELECT MAX(id_cuenta) from registro_cuenta)';
+               $resp = $conn->query($sql);
+               $idCuenta = $resp->fetch_assoc();
 
-                    // require_once('includes/funciones/conexion.php');
-                    // $stmt = $conn->prepare("INSERT INTO registro_cuenta (id_cuenta,nombre_user,ap_user,am_user,email_user,password_user) VALUES (?,?,?,?,?,?)");
-                    // $stmt->bind_param("isssss",$id, $nombre,$ap_user,$am_user,$email,$password);
-                    // $stmt->execute();
-                    // $stmt->close();
-                    // $conn->close();
-          
-          
-               }catch(Exception  $e){
-          
-                    $error = $e->getMesage();
-          
-          
-               }
+               ///Se obtiene  el ultimo registro agregado a la base de datos de registrar cuenta
+               $id =  $idCuenta['id_cuenta'];
+               $id = $id+1;
+               $nombre = $_POST['nombre'];
+               $ap_user = $_POST['ap-registrar'];
+               $am_user = $_POST['am-registrar'];
+               $email = $_POST['email'];
+               $password = $_POST['password'];
+
+               $stmt = $conn->prepare("INSERT INTO registro_cuenta (id_cuenta,nombre_user,ap_user,am_user,email_user,password_user) VALUES (?,?,?,?,?,?)");
+               $stmt->bind_param("isssss",$id, $nombre,$ap_user,$am_user,$email,$password);
+               $stmt->execute();
+               $stmt->close();
+                  
+             
+               $conn->close();
+
+
+          }catch(Exception $e){
+
+               $error = $e->getMesage();
+              
+
+          }
+
      }
 
 
@@ -150,6 +157,5 @@
           ?>
      </pre>
 <?php endif;?>
-
 
 <?php require_once 'includes/templates/footer.php';?>
