d31bf12316ea0999d07b86d0b5d26d92dc38afe3
migue <migjacobo73@gmail.com>
2021-03-07 16:27:54 -0600

    Update datos.php

--- includes/funciones/datos.php
+++ includes/funciones/datos.php
@@ -1,10 +1,18 @@
 <?php 
   include("conexion.php");
-  $talla=$_POST['talla'];
-  $marca=$_POST['marca'];
-  $modelo=$_POST['modelo'];
    
-       $consulta = mysqli_query($conn,"SELECT precio FROM catalogo WHERE marca = '$marca' AND modelo = '$modelo' AND corrida = '$talla'");
+    if (isset($_POST['opcionPrecio'])) {
+
+      $opcionPrecio=$_POST['opcionPrecio'];
+      $var1='opcionPrecio';
+
+    if(strcmp($opcionPrecio, $var1) === 0){
+      
+      $talla=$_POST['talla'];
+      $marca=$_POST['marca'];
+      $modelo=$_POST['modelo'];
+
+        $consulta = mysqli_query($conn,"SELECT precio FROM catalogo WHERE marca = '$marca' AND modelo = '$modelo' AND corrida = '$talla'");
 
        $extraido= mysqli_fetch_array($consulta);
        if(empty($extraido)){
@@ -12,6 +20,28 @@
        } else{
         echo "".$extraido['precio'];
        }
+    }
        
-      
+    } 
+    else if(isset($_POST['buscaTalla'])){
+
+      $buscaTalla=$_POST['buscaTalla'];
+      $var2='buscaTalla';
+
+      if(strcmp($buscaTalla, $var2) === 0){
+
+      $marca=$_POST['marca'];
+      $modelo=$_POST['modelo'];
+
+      $consulta = mysqli_query($conn,"SELECT DISTINCT corrida FROM catalogo WHERE marca = '$marca' AND modelo = '$modelo'");
+
+      $extraido= mysqli_fetch_array($consulta);
+      if(empty($extraido)){
+        echo "MAL";
+        //print_r ($extraido);
+      } else{
+        echo json_encode($extraido);
+      }
+    }
+  }
 ?>
