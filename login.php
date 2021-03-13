b10785c4268d86459dcb3c34fc889ad58f45c021
Neto <contacto@exoft.com>
2021-03-12 19:49:39 -0600

    login icono password
    password

--- login.php
+++ login.php
@@ -8,29 +8,26 @@
                 <img src="img/La casa del catalogo2.0.png" alt="logo" width="20%">
             </div>
             <div class="contacto">
-                <form action="#" id="formulario">
+                <form action="includes/funciones/datos.php" id="formulario" method="post">
                     <div class="row justify-content-center">
                         <div class="campo-contacto col-md-12">
                             <i class="fas fa-at"></i>
-                            <input type="email" id="email" placeholder="Email">
+                            <input type="email" id="email" placeholder="Email" name="email">                           
                         </div>
                         <div class="campo-contacto col-md-12 ">
-                            <i class="fas fa-lock"></i> 
-                           <input type="password" id="password" placeholder="password">
+                            <i class="fas fa-lock"></i>
+                           <input type="password" id="password" placeholder="password" name ='password' class="password-login">
+                           <i class="fas fa-eye-slash mostrar-password-login" id="password-login"></i>
                         </div>
                     </div>
                        
     
                     <div class="row justify-content-center">
                         <div class="boton-login">
-                            <button type="submit" id="btn-login" class="btn btn-dark">Login</button>
-    
+                            <button type="submit" id="btn-login" class="btn btn-dark" name="submit">Login</button>
                         </div>
-    
-
                     </div>
                       
-
                         <div class="seccion-info">
                             <div class="cuenta">
                                 <p>No tienes cuenta? <span> <a href="registrar.php">Crear Cuenta</a></span> </p>
@@ -40,7 +37,11 @@
     
                             </div>
                         </div>
+                      
+                 <!-- Codigo php -->
                 </form>
+                
+
             </div>
         
         </div>
@@ -49,7 +50,7 @@
     <?php include_once 'includes/templates/footer.php'; ?>
   
 
-    <script src="js/js/login.js" type="module" ></script>
+    <script src="js/js/login.js" type="module"></script>
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </body>
