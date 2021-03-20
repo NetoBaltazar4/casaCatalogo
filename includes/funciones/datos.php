ae6c3b35105f2ba73023b92125babac5193e1bcc
migue <migjacobo73@gmail.com>
2021-03-20 15:28:31 -0600

    Update datos.php

--- includes/funciones/datos.php
+++ includes/funciones/datos.php
@@ -1,49 +1,102 @@
 <?php 
   include("conexion.php");
    
-    if (isset($_POST['opcionPrecio'])) {
+       if (isset($_POST['opcionPrecio'])) {
 
       $opcionPrecio=$_POST['opcionPrecio'];
       $var1='opcionPrecio';
 
     if(strcmp($opcionPrecio, $var1) === 0){
+
+      //Preparamos La Sentencia
+      $sentencia = $conn->prepare("SELECT precio FROM catalogo WHERE marca = ?  AND modelo = ? AND corrida = ?");
       
+      //Extraemos datos
       $talla=$_POST['talla'];
       $marca=$_POST['marca'];
       $modelo=$_POST['modelo'];
 
-        $consulta = mysqli_query($conn,"SELECT precio FROM catalogo WHERE marca = '$marca' AND modelo = '$modelo' AND corrida = '$talla'");
+      //Indicamos los valores pasados por referencia
+      $sentencia->bind_param("sss", $marca, $modelo, $talla);
 
-       $extraido= mysqli_fetch_array($consulta);
-       if(empty($extraido)){
-         echo "MAL";
-       } else{
-        echo "".$extraido['precio'];
-       }
+      if($sentencia->execute()){
+	
+        //Alamacenaos los datos de la consulta
+        $sentencia->store_result();
+        
+        if($sentencia->num_rows == 0){		
+          echo "MAL";
+        }
+        
+        //Indicamos la variable donde se guardaran los resultados
+        $sentencia->bind_result($res);
+        
+        //listamos todos los resultados
+        while($sentencia->fetch()){
+          echo "$res";
+        }
+        
+        //Cerramos la conexion
+        $sentencia->close();
+        
+      } else {
+        exit('Error al realizar la consulta: '.$sentencia->close());
+      }
     }
        
-    } 
-    else if(isset($_POST['buscaTalla'])){
+    } else if(isset($_POST['buscaTalla'])){
 
       $buscaTalla=$_POST['buscaTalla'];
       $var2='buscaTalla';
 
       if(strcmp($buscaTalla, $var2) === 0){
 
+      //Preparamos La Sentencia
+      $sentencia = $conn->prepare("SELECT corrida FROM catalogo WHERE marca = ?  AND modelo = ?");
+
+      //Extraemos datos
       $marca=$_POST['marca'];
       $modelo=$_POST['modelo'];
 
-      $consulta = mysqli_query($conn,"SELECT DISTINCT corrida FROM catalogo WHERE marca = '$marca' AND modelo = '$modelo'");
+      //Indicamos los valores pasados por referencia
+      $sentencia->bind_param("ss", $marca, $modelo);
 
-      $extraido= mysqli_fetch_array($consulta);
-      if(empty($extraido)){
-        echo "MAL";
-        //print_r ($extraido);
-      } else{
-        echo json_encode($extraido);
+      if($sentencia->execute()){
+	
+        //Alamacenaos los datos de la consulta
+        $sentencia->store_result();
+        
+        if($sentencia->num_rows == 0){		
+          echo "MAL";
+        }
+        
+        //Indicamos la variable donde se guardaran los resultados
+        $sentencia->bind_result($res);
+
+        //Inicialisamos variable i en 0 para derle valor al arreglo en el ciclo while
+        $i=0;
+
+        //Creamos arreglo
+        $rawdata = array();
+
+        //listamos todos los resultados para guardarlos en el arreglo
+        while($sentencia->fetch()){
+          $rawdata[$i] = $res;
+          $i++;
+        }
+
+        //Transformamos el arreglo a formato JSON y lo enviamos
+        echo json_encode($rawdata);
+        
+        //Cerramos la conexion
+        $sentencia->close();
+        
+      } else {
+        exit('Error al realizar la consulta: '.$sentencia->close());
       }
+     
     }
-  } else if(isset($_POST['passANDemail'])){
+  }  else if(isset($_POST['passANDemail'])){
 
    
     $email = $_POST['email'];
